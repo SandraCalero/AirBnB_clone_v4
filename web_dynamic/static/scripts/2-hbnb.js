d6779546f8f05939f4078cc9efab8ae987c395b7
Nel10 <3461@holbertonschool.com>
2022-02-10 11:18:20 -0500

    create files 2-hbnb.py, 2-hbnb.html, 2-hbnb.js and update app.py

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  const dictChecked = {};
+  $('input[type=checkbox]').change(function () {
+    if (this.checked) {
+      const key = $(this).attr('data-id');
+      const value = $(this).attr('data-name');
+      dictChecked[key] = value;
+    } else {
+      const key = $(this).attr('data-id');
+      delete dictChecked[key];
+    }
+    const values = Object.keys(dictChecked).map(function (key) {
+      return dictChecked[key];
+    });
+    $('div.amenities h4').text(values.join(', '));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
+});
