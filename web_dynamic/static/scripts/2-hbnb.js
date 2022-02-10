88a8757e837a74a2ba4c40f81dc1ae876d886dcf
Sandra Calero <3358@holbertonschool.com>
2022-02-10 14:44:51 -0500

    update 3-header.css with new id and class and 2-hbnb.js to delete textStatus

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -14,13 +14,11 @@ $(document).ready(function () {
     });
     $('div.amenities h4').text(values.join(', '));
   });
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
-    if (textStatus === 'success') {
-      if (data.status === 'OK') {
-        $('#api_status').addClass('available');
-      } else {
-        $('#api_status').removeClass('available');
-      }
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
     }
   });
 });
