619e3b9e2f837b35a03e1c5fe1d3f00ef3e533e8
Sandra Calero <3358@holbertonschool.com>
2022-02-10 21:11:00 -0500

    add 3-hbnb.py, 3-hbnb.js and 3-hbnb.html and update 6-filters.css

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+  const dictChecked = {};
+  $('input[type=checkbox]').change(function () {
+    if (this.checked) {
+      const key = $(this).attr('data-id');
+      const value = $(this).attr('data-name');
+      dictChecked[key] = value;
+    } else {
+      const key = $(this).attr('data-id');
+      delete dictChecked[key];
+    }
+    const values = Object.keys(dictChecked).map(function (key) {
+      return dictChecked[key];
+    });
+    $('div.amenities h4').text(values.join(', '));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    type: 'POST',
+    data: '{}',
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    success: function (searchPlaces) {
+      for (const place of searchPlaces) {
+        $('section.places').append(`<article>
+            <div class='title_box'>
+              <h2>${place.name}</h2>
+              <div class='price_by_night'>$${place.price_by_night}</div>
+            </div>
+            <div class='information'>
+              <div class='max_guest'>${place.max_guest} Guest${
+          place.max_guest !== 1 ? 's' : ''
+        }</div>
+                  <div class='number_rooms'>${place.number_rooms} Bedroom${
+          place.number_rooms !== 1 ? 's' : ''
+        }</div>
+                  <div class='number_bathrooms'>${
+                    place.number_bathrooms
+                  } Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+            </div>
+            <div class='user'>
+                  <b>Owner:</b>
+                </div>
+                <div class='description'>
+              ${place.description}
+                </div>
+          </article>`);
+      }
+    }
+  });
+});
