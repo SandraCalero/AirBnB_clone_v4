db71a83495f7c4803ae6acf5fdf85a69a2e25577
Sandra Calero <3358@holbertonschool.com>
2022-02-11 12:08:54 -0500

    impove repited code in 4-hbnb.js

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -21,36 +21,37 @@ $(document).ready(function () {
       $('#api_status').removeClass('available');
     }
   });
+  const appendSearch = function (searchPlaces) {
+    for (const place of searchPlaces) {
+      $('section.places').append(`<article>
+          <div class='title_box'>
+            <h2>${place.name}</h2>
+            <div class='price_by_night'>$${place.price_by_night}</div>
+          </div>
+          <div class='information'>
+            <div class='max_guest'>${place.max_guest} Guest${
+        place.max_guest !== 1 ? 's' : ''
+      }</div>
+                <div class='number_rooms'>${place.number_rooms} Bedroom${
+        place.number_rooms !== 1 ? 's' : ''
+      }</div>
+                <div class='number_bathrooms'>${
+                  place.number_bathrooms
+                } Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+          </div>
+              <div class='description'>
+            ${place.description}
+              </div>
+        </article>`);
+    }
+  };
   $.ajax({
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
     type: 'POST',
     data: '{}',
     contentType: 'application/json; charset=utf-8',
     dataType: 'json',
-    success: function (searchPlaces) {
-      for (const place of searchPlaces) {
-        $('section.places').append(`<article>
-            <div class='title_box'>
-              <h2>${place.name}</h2>
-              <div class='price_by_night'>$${place.price_by_night}</div>
-            </div>
-            <div class='information'>
-              <div class='max_guest'>${place.max_guest} Guest${
-          place.max_guest !== 1 ? 's' : ''
-        }</div>
-                  <div class='number_rooms'>${place.number_rooms} Bedroom${
-          place.number_rooms !== 1 ? 's' : ''
-        }</div>
-                  <div class='number_bathrooms'>${
-                    place.number_bathrooms
-                  } Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
-            </div>
-                <div class='description'>
-              ${place.description}
-                </div>
-          </article>`);
-      }
-    }
+    success: appendSearch
   });
   $('#search').click(function () {
     $('article').remove();
@@ -61,30 +62,7 @@ $(document).ready(function () {
       data: JSON.stringify(jsonRequest),
       contentType: 'application/json; charset=utf-8',
       dataType: 'json',
-      success: function (searchPlaces) {
-        for (const place of searchPlaces) {
-          $('section.places').append(`<article>
-              <div class='title_box'>
-                <h2>${place.name}</h2>
-                <div class='price_by_night'>$${place.price_by_night}</div>
-              </div>
-              <div class='information'>
-                <div class='max_guest'>${place.max_guest} Guest${
-            place.max_guest !== 1 ? 's' : ''
-          }</div>
-                    <div class='number_rooms'>${place.number_rooms} Bedroom${
-            place.number_rooms !== 1 ? 's' : ''
-          }</div>
-                    <div class='number_bathrooms'>${
-                      place.number_bathrooms
-                    } Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
-              </div>
-                  <div class='description'>
-                ${place.description}
-                  </div>
-            </article>`);
-        }
-      }
+      success: appendSearch
     });
   });
 });
